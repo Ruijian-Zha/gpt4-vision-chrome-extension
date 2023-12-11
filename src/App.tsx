ee5916351dcb565a2e19486d17dce58692bcc18f
Ruijian-Zha <1323318674@qq.com>
2023-12-10 20:21:10 -0500

    MUI feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useRef, useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import {
   getChromeStorage,
 } from "./extension-helper";
@@ -8,8 +15,10 @@ import { ToolResult } from "./tool-controller";
 import { ChatBubble } from "./ChatBubble";
 
 export const App: React.FC = () => {
-  const [input, setInput] = React.useState<string>("");
-  const [hasPreviousState, setHasPreviousState] = React.useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [hasPreviousState, setHasPreviousState] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     getChromeStorage("chatData").then((chatData) => {
@@ -19,28 +28,60 @@ export const App: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const input = (e.currentTarget.elements[0] as HTMLInputElement).value;
-    setInput(input);
+    if (textareaRef.current) {
+      const inputValue = textareaRef.current.value;
+      setInput(inputValue); 
+    }
   };
+  
 
   return (
-    <div className="flex flex-col gap-4 p-3 w-96">
-      <h1 className="text-2xl font-bold">GPT4-Vision Assistant</h1>
-      <form className="flex flex-col" onSubmit={handleSubmit}>
-        {(input || hasPreviousState) && <AIChat input={input} />}
-        <div className="flex gap-2">
-          <textarea
-            className="textarea textarea-bordered h-24 leading-3"
-            placeholder="Enter task description"
-          />
-          <button className="btn btn-primary flex-grow" type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+    
+    <Box sx={{ width: '100%', typography: 'body1', margin: ['10px', '10px', '10px', '10px'] }}>
+      <TabContext value={"1"}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={() => {}} aria-label="lab API tabs example">
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+    
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            {(input || hasPreviousState) && <AIChat input={input} />}
+            <Box>
+              <TextField
+                label="Your Request"
+                variant="outlined"
+                fullWidth
+                inputRef={textareaRef}
+                multiline
+                margin="normal"
+                placeholder="Enter task description"
+              />
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                type="submit"
+                disabled={loading}
+              >
+                Submit
+              </Button>
+            </Box>
+          </form>
+        </TabPanel>
+        
+        <TabPanel value="2">
+        Item Two
+        </TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+
   );
-}
+};
 
 // whenever the data changes, scroll down to the last message
 const AIChat = ({ input }: { input: string }) => {
